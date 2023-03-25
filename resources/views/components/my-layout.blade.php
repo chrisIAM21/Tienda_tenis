86f334eac2226b01a01c41b828d586ad9d951d6e
Christopher Iván Andrade Mendoza <christopher.andrade4604@alumnos.udg.mx>
2023-03-24 21:03:28 -0600

    Añadí botón de iniciar sesión y registrarse+icono

--- resources/views/components/my-layout.blade.php
+++ resources/views/components/my-layout.blade.php
@@ -87,23 +87,25 @@
                             <span class="navbar-toggler-bar navbar-kebab"></span>
                         </button>
                         <div class="collapse navbar-collapse justify-content-end" id="navigation">
-                            <form>
-                                <div class="input-group no-border">
-                                    <input type="text" value="" class="form-control" placeholder="Buscar...">
-                                    <div class="input-group-append">
-                                        <div class="input-group-text">
-                                            <i class="now-ui-icons ui-1_zoom-bold"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
+                            
                             <ul class="navbar-nav">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">
+                                    <a class="nav-link" href="/login">
                                         <i class="now-ui-icons users_single-02"></i>
-                                        <p>
-                                            <span class="d-lg-none d-md-block">Usuario</span>
-                                        </p>
+                                        <p>Iniciar sesión</p>
+                                            <!--<span class="d-lg-none d-md-block">Usuario</span>
+                                        </p>-->
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/register">
+                                        <i class="now-ui-icons"><svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"></path>
+                                        </svg></i>
+                                        
+                                        <p>Registrarse</p>
+                                            <!--<span class="d-lg-none d-md-block">Usuario</span>
+                                        </p>-->
                                     </a>
                                 </li>
                             </ul>
