b9f0f33f9823740bbc25a326566f104e7ac65721
Christopher Iván Andrade Mendoza <christopher.andrade4604@alumnos.udg.mx>
2023-05-17 15:14:32 -0600

    Corrección para que aparezca Register en Inicio

--- resources/views/components/layout_inicio.blade.php
+++ resources/views/components/layout_inicio.blade.php
@@ -285,21 +285,20 @@
                                                     @csrf
                                                 </form>
                                             @else --}}                                        
-                                        @if (!Route::has('login'))
-                                            @auth
-                                                <a class="btn btn-sm btn-outline-primary btn-round mb-0 me-1 mt-2 mt-md-0"
-                                                    href="/register">
-                                                    <i class="now-ui-icons"><svg fill="none" stroke="currentColor"
-                                                            stroke-width="1.5" viewBox="0 0 16 21"
-                                                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
-                                                            height="16" width="21">
+                                        @if (Route::has('register'))
+                                            @guest
+                                                <a class="btn btn-sm btn-outline-primary btn-round mb-0 me-1 mt-2 mt-md-0" href="/register">
+                                                    <i class="now-ui-icons">
+                                                        <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 16 21"
+                                                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true" height="16" width="21">
                                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                                 d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z">
                                                             </path>
-                                                        </svg></i>
-                                                    Registrate
+                                                        </svg>
+                                                    </i>
+                                                    Regístrate
                                                 </a>
-                                            @endauth
+                                            @endguest
                                         @endif
                                     </li>
 
